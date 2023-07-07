0cb686589e07fc70c8abaa3e8aa0bb0a4b82bad9
Titou360 <clement.felices@gmail.com>
2023-07-07 14:10:43 +0200

    page de login ok html + css

--- FrontEnd/assets/JS/script.js
+++ FrontEnd/assets/JS/script.js
@@ -1,87 +1,105 @@
 document.addEventListener('DOMContentLoaded', function() {
-let titleProjects = "Mes Projets"
-let portfolio = document.querySelector('#portfolio')
+  let titleProjects = "Mes Projets";
+  let portfolio = document.querySelector('#portfolio');
 
-let html = `
+  let html = `
     <h2>${titleProjects}</h2>
-      <div class="filters"></div>
-      <div class="gallery"></div>
-`
-portfolio.innerHTML = html
-});
+    <div class="filters"></div>
+    <div class="gallery"></div>
+  `;
+  portfolio.innerHTML = html;
 
-fetch("http://localhost:5678/api/works")
-  .then(response => response.json())
-  .then(data => {
-    // Je génére et rempli les figures pour chaque projet
-    data.forEach(function(projet) {
-      genererFigure(projet);
+  fetch("http://localhost:5678/api/works")
+    .then(response => response.json())
+    .then(data => {
+      // Je génère et remplis les figures pour chaque projet
+      data.forEach(function(projet) {
+        genererFigure(projet);
+      });
+
+      // Récupérer toutes les catégories uniques
+      const categories = new Set();
+      categories.add("Tous");
+      data.forEach(projet => {
+        categories.add(projet.category.name);
+      });
+
+      // Sélectionner le conteneur des filtres
+      const filtersContainer = document.querySelector('.filters');
+
+      // Créer un bouton pour chaque catégorie
+      const allFigures = document.querySelectorAll('.gallery figure');
+      const filterButtons = [];
+
+      categories.forEach(category => {
+        const button = document.createElement('button');
+        const buttonClass = 'btn-filter';
+        button.classList.add(buttonClass);
+        button.textContent = category;
+        filtersContainer.appendChild(button);
+        filterButtons.push(button);
+
+        // Ajouter une classe spécifique pour le bouton "Tous"
+        if (category === "Tous") {
+          button.classList.add('filter-tous');
+        } else {
+          const categoryClass = 'filter-' + encodeURIComponent(category.toLowerCase().replace(' ', '-'));
+          button.classList.add(categoryClass);
+        }
+      });
+
+      // Gestionnaire d'événement pour les boutons de filtre
+      filterButtons.forEach(button => {
+        button.addEventListener('click', function() {
+          // Supprimer la classe "data-active" de tous les boutons
+          filterButtons.forEach(btn => {
+            btn.classList.remove('data-active');
+          });
+
+          // Ajouter la classe "data-active" au bouton cliqué
+          button.classList.add('data-active');
+
+          // Récupérer la classe du bouton cliqué (correspond à la catégorie)
+          const categoryClass = button.classList[1];
+
+          // Afficher ou masquer les figures en fonction de la catégorie
+          allFigures.forEach(figure => {
+            const figureCategories = Array.from(figure.classList).filter(className => className.startsWith('filter-'));
+            const shouldDisplay = categoryClass === 'filter-tous' || figureCategories.includes(categoryClass);
+
+            figure.style.display = shouldDisplay ? 'block' : 'none';
+          });
+        });
+      });
+    })
+    .catch((error) => {
+      console.log("Une erreur s'est produite lors de la récupération des données :", error);
     });
-  })
-  // J'établie un message d'erreur en cas de soucis de connexion
-  .catch((error) => {
-    console.log("Une erreur s'est produite lors de la récupération des données :", error);
-  });
 
-  // Je crée la fonction 
-function genererFigure(projet) {
-  // Je crée un nouvel élément <figure>
-  let figureElement = document.createElement('figure');
+  // Je crée la fonction
+  function genererFigure(projet) {
+    // Je crée un nouvel élément <figure>
+    let figureElement = document.createElement('figure');
 
-  // Je crée un nouvel élément <img> avec les attributs src et alt
-  let imgElement = document.createElement('img');
-  imgElement.src = projet.imageUrl;
-  imgElement.alt = projet.title;
+    // Ajouter une classe à la figure en fonction de la catégorie
+    let categoryClass = 'filter-' + encodeURIComponent(projet.category.name.toLowerCase().replace(' ', '-'));
+    figureElement.classList.add(categoryClass);
 
-  // Je crée un nouvel élément <figcaption> avec le titre du projet
-  let figcaptionElement = document.createElement('figcaption');
-  figcaptionElement.textContent = projet.title;
+    // Je crée un nouvel élément <img> avec les attributs src et alt
+    let imgElement = document.createElement('img');
+    imgElement.src = projet.imageUrl;
+    imgElement.alt = projet.title;
 
-  // J'ajoute les éléments à l'élément <figure>
-  figureElement.appendChild(imgElement);
-  figureElement.appendChild(figcaptionElement);
+    // Je crée un nouvel élément <figcaption> avec le titre du projet
+    let figcaptionElement = document.createElement('figcaption');
+    figcaptionElement.textContent = projet.title;
 
-  // J'ajoute enfin l'élément <figure> à la class .gallery
-  let containerElement = document.querySelector('.gallery');
-  containerElement.appendChild(figureElement);
-}
+    // J'ajoute les éléments à l'élément <figure>
+    figureElement.appendChild(imgElement);
+    figureElement.appendChild(figcaptionElement);
 
-
-
-fetch("http://localhost:5678/api/works")
-.then(response => response.json())
-.then(data => {
-  // Récupérer toutes les catégories uniques
-  const categories = new Set();
-  categories.add("Tous");
-  data.forEach(projet => {
-    categories.add(projet.category.name);
-  });  
-
-  // Sélectionner le conteneur des filtres
-  const filtersContainer = document.querySelector('.filters');
-
-  // Créer un bouton pour chaque catégorie
-  categories.forEach(category => {
-    const button = document.createElement('button');
-    button.classList.add('btn-filter');
-    button.textContent = category;
-    filtersContainer.appendChild(button);
-
-    button.addEventListener('click', function() {
-      const filterButtons = document.querySelectorAll('.btn-filter');
-      filterButtons.forEach(btn => {
-        btn.classList.remove('data-active');
-      })
-      button.classList.add('data-active');
-  });
-
-  // Important, to be place at the end
-  data.forEach(projet => {
-    genererFigure(projet);
-  });
-})
-.catch((error) => {
-  console.log("Une erreur s'est produite lors de la récupération des données :", error);
+    // J'ajoute enfin l'élément <figure> à la class .gallery
+    let containerElement = document.querySelector('.gallery');
+    containerElement.appendChild(figureElement);
+  }
 });
-})
